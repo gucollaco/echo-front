166d1d6510f14fc91a07be4f01f7c194b05859f0
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2019-11-02 13:29:28 -0300

    Bottom nav on layouts

---
+++ components/BottomNav.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <v-bottom-navigation
+            :value="activeBtn"
+            grow
+            fixed
+        >
+            <v-btn>
+            <span>Home</span>
+            <v-icon>mdi-history</v-icon>
+            </v-btn>
+
+            <v-btn>
+            <span>Favorites</span>
+            <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn>
+            <span>Nearby</span>
+            <v-icon>mdi-map-marker</v-icon>
+            </v-btn>
+        </v-bottom-navigation>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+    name: "BottomNav"
+}
+</script>
+
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,23 @@
 <template>
-  <v-bottom-navigation
-    :value="activeBtn"
-    grow
-    fixed
-  >
-    <v-btn>
-      <span>Home</span>
-      <v-icon>mdi-history</v-icon>
-    </v-btn>
-
-    <v-btn>
-      <span>Favorites</span>
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-
-    <v-btn>
-      <span>Nearby</span>
-      <v-icon>mdi-map-marker</v-icon>
-    </v-btn>
-  </v-bottom-navigation>
+    <div>
+        <nuxt />
+        <BottomNav />
+    </div>
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
 
 </style>
+
 <script>
-  export default {
+import BottomNav from '../components/BottomNav'
+
+export default {
+    components: { BottomNav },
     data () {
-      return {
-        activeBtn: 1,
-      }
-    },
-  }
+        return {
+            activeBtn: 1,
+        }
+    }
+}
 </script>
