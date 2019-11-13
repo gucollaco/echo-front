dd885250af8d756ec706e3fd1534f88c0f816c84
Gustavo Martins Collaço <gustavo.collaco@sistemapoliedro.com.br>
2019-11-13 14:28:13 -0300

    Calendar page ok

--- pages/calendar/index.vue
+++ pages/calendar/index.vue
@@ -1,36 +1,29 @@
 <template>
   <v-container fluid>
-    <v-row dense>
+    <v-row justify="space-around">
       <v-col cols="12">
-        <v-card color="#385F73" dark>
-          <v-card-title class="headline">Unlimited music now</v-card-title>
-
-          <v-card-subtitle
-            >Listen to your favorite artists and albums whenever and wherever,
-            online and offline.</v-card-subtitle
-          >
-
-          <v-card-actions>
-            <v-btn text>Listen Now</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col v-for="(item, i) in items" :key="i" cols="12">
-        <v-card :color="item.color" dark>
-          <div class="d-flex flex-no-wrap justify-space-between">
-            <div>
-              <v-card-title class="headline" v-text="item.title"></v-card-title>
-
-              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-            </div>
-
-            <v-avatar class="ma-3" size="125" tile>
-              <v-img :src="item.src"></v-img>
-            </v-avatar>
-          </div>
-        </v-card>
+        <v-date-picker
+          v-model="picker2"
+          full-width
+          color="primary darken-3"
+          header-color="primary"
+          locale="pt-br"
+        ></v-date-picker>
       </v-col>
     </v-row>
+    <v-card color="primary darken-1" dark>
+      <v-card-title>
+        Interação Humano-Computador
+      </v-card-title>
+      <v-card-subtitle>
+        Sala 404<br />
+        Qua 19:00 - 23:00
+      </v-card-subtitle>
+      <v-card-actions class="mt-n6">
+        <v-spacer></v-spacer>
+        <v-icon>mdi-book-variant</v-icon>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
@@ -38,20 +31,7 @@
 export default {
   layout: "menu",
   data: () => ({
-    items: [
-      {
-        color: "#1F7087",
-        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-        title: "Supermodel",
-        artist: "Foster the People"
-      },
-      {
-        color: "#952175",
-        src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
-        title: "Halcyon Days",
-        artist: "Ellie Goulding"
-      }
-    ]
+    picker2: new Date().toISOString().substr(0, 10)
   })
 }
 </script>
