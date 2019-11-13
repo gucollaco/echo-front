c7bcb26b271da345868e2d0a5dadf6bd421d77ef
dsalexan <danilo.salexan@gmail.com>
2019-11-13 10:17:21 -0300

    struturing

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from 'vuetify/es5/util/colors'
+
 
 export default {
   mode: 'spa',
@@ -47,6 +49,27 @@ export default {
   modules: [
     '@nuxtjs/pwa',
   ],
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      light: true,
+      themes: {
+        light: {
+          primary: colors.teal,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
   /*
   ** Build configuration
   */
